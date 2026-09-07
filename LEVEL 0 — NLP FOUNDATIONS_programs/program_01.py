f06e8994f28c7bd2a7f6fe26075f19f7a35c079c
Rohith Itagi <106232247+Rohith-Itagi7@users.noreply.github.com>
2026-09-07 12:40:57 +0530

    Create program_01.py

---
+++ LEVEL 0 — NLP FOUNDATIONS_programs/program_01.py
@@ -0,0 +1,52 @@
+# Program 01 — Basic NLP Text Preprocessing Pipeline
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import string
+
+# Run these once
+nltk.download("punkt")
+nltk.download("stopwords")
+nltk.download("wordnet")
+nltk.download("omw-1.4")
+
+text = "Natural Language Processing is amazing! It helps computers understand human languages."
+
+# 1. Lowercase
+text = text.lower()
+
+# 2. Remove punctuation
+text = text.translate(str.maketrans("", "", string.punctuation))
+
+# 3. Tokenization
+tokens = word_tokenize(text)
+
+# 4. Remove stopwords
+stop_words = set(stopwords.words("english"))
+
+filtered_words = [
+    word for word in tokens
+    if word not in stop_words
+]
+
+# 5. Lemmatization
+lemmatizer = WordNetLemmatizer()
+
+lemmatized_words = [
+    lemmatizer.lemmatize(word)
+    for word in filtered_words
+]
+
+print("Original Text:")
+print(text)
+
+print("\nTokens:")
+print(tokens)
+
+print("\nAfter Stopword Removal:")
+print(filtered_words)
+
+print("\nAfter Lemmatization:")
+print(lemmatized_words)
